7dc10fb15fe690bb8007277ec863f50dfaa1402d
TabithaAluel <alueltabitha@gmail.com>
2021-05-31 20:08:29 +0000

    python

--- bank.py
+++ bank.py
@@ -1,40 +1,58 @@
+from datetime import datetime
 class Bank:
 
-    def __init__  (self,name,phone_number):
+    def __init__(self,name,phoneNumber):
         self.name=name
-        self.phone_number=phone_number
+        self.phoneNumber=phoneNumber
         self.balance=0
+        self.statement=[]
 
-    def show_balance(self): 
-        return f" Hello {self.name}your balance is {self.balance}"
+    def show_balance(self):
+        return f"Hello {self.name},your balance is {self.balance}."
 
     def deposit(self,amount):
-        if amount>self.balance:
-            return f"Hello {self.name} your balance is {self.balance}"
+        if(amount<0):
+            return f"You cannot deposit money less than 0"
         else:
-            self.balance-=amount  
-        return self.show_balance()
-        
-    def withdraw(self,amount):
-        self.amount=amount
-        if amount > self.balance:
-            return f"Your balance is {self.balance} you cannot withdraw {amount}"
-        else:
-            self.balance=amount
+            self.balance+=amount
+            now=datetime.now()
+            transaction={"amount":amount,"time":now,"narration":"You deposited"}
+            self.statement.append(transaction)
+
+
             return self.show_balance()
-            
-    def borrow(self,amount):  
-        return f"Hello {self.name} you have borrowed a loan of {amount}" 
-
-    def repay_loan(self,amount):
-        return f"Hello {self.name} you have repaid a loan of {amount}" 
-            
 
 
+    def show_statement(self):
+        for transaction in self.statement:
+            amount=transaction["amount"]
+            narration=transaction["narration"]
+            time=transaction["time"]
+            date=time.strftime("%d/ %m / %y")
+            print(f"{date}:{narration}:{amount}")
+
+
+    def withdraw(self,amount):
+        if(amount>self.balance):
+            return f"Your balance is {self.balance}.You cannot withdraw {amount}"
+        else:
+            self.balance-=amount
+            now=datetime.now()
+            transaction={"amount":amount,"time":now,"narration":"You have withdrawn"}
+            self.statement.append(transaction)
+
+            return self.show_balance()
+
+    def borrow(self,amount):
+        self.amount=amount
+        if (amount>0):
+            return f"You can borrow{amount}"
+        else:
+            return f"You cannot borrow another"
     
-            
-         
-
-                
-
-
+    def repay_loan(self,amount):
+        self.amount=amount
+        if(amount>0):
+            return f"Please repay your loan of{amount}"
+        else:
+            return f"Your loan has been  paid!"
