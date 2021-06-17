ea8e4e4155ab9059c7d6290511c161e22900d64c
TabithaAluel <alueltabitha@gmail.com>
2021-06-17 11:15:01 +0000

    test

--- pyquiz/python_text.py
+++ pyquiz/python_text.py
@@ -1,29 +1,25 @@
 x=[100,110,120,140,150]
-# for a in x :
-#     a*a in x
-#     print(a)
+for a in x :
+    a*a in x
+    print(a)
 
-#     n=0
-#     x=range(1,n) 
-#     for i in x:
-#      i%3==0 in x
-#     print(i)
+    n=0
+    x=range(1,n) 
+    for i in x:
+     i%3==0 in x
+    print(i)
 
-# x = [[1,2],[3,4],[5,6]]
-# y=[1,2]+[3,4]+[5,6]
-# print(y)
+x = [[1,2],[3,4],[5,6]]
+y=[1,2]+[3,4]+[5,6]
+print(y)
 
-# x = ['a','b','a','e','d','b','c','e','f','g','h']
-# y=x(set)
-# print(y)
+x = ['a','b','a','e','d','b','c','e','f','g','h']
+y=x(set)
+print(y)
 
 x=range(100,200)
 for i in x:
-    if i%7==0 in x:
-       print("The number is divisible by 7".format(i))
-    else:
-        print("The number is not divisible by 7".format(i))
-    
+    print(i)
 def greet(age):
     name='Eunice'
     year_of_birth=2021-age
